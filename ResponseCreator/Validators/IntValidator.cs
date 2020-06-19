c4542a8812495c16d070a8b284f4fb53674db5da
pred <adrian111217@gmail.com>
2020-06-19 09:41:58 +0200

    dev

---
+++ ResponseCreator/Validators/IntValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ResponseCreator.Abstract;
+
+namespace ResponseCreator.Validators
+{
+    public class IntValidator : BaseTypeValidator<int>
+    {
+        public IntValidator(int objectUnderValidation, IResponseCreator responseCreator, string key, string keyPrefix = null) : base(objectUnderValidation, responseCreator, key, keyPrefix)
+        {
+        }
+
+        public IntValidator Required(string customMessage = null)
+        {
+            
+        }
+    }
+}
